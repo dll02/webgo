d0abc3863923a87c0541983e5600858b1b477d29
1043532278 <1043532278@qq.com>
2023-08-24 16:22:50 +0800

    lesson-6: add server shutdown connection closed gracefully

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"webgo/framework"
 	"webgo/framework/middleware"
-	"net/http"
 )
 
 const shortDuration = 1 * time.Millisecond
 
 func main() {
 	core := framework.NewCore()
-	// 注册core 上的中间件 
+	// 注册core 上的中间件
 	core.Use(middleware.Recovery())
 	core.Use(middleware.Cost())
 
@@ -20,5 +25,24 @@ func main() {
 		Handler: core,
 		Addr:    ":8888",
 	}
-	server.ListenAndServe()
+
+	// 启动服务的 Goroutine
+	go func() {
+		server.ListenAndServe()
+	}()
+	// 当前的 Goroutine 等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	// timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
 }
--- user_controller.go
+++ user_controller.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"webgo/framework"
 )
 
 
 func UserLoginController(c *framework.Context) error {
 	foo, _ := c.QueryString("foo", "def")
+	fmt.Println("start wait")
+	// 等待10s才结束执行  
+	time.Sleep(10 * time.Second)
 	// 打印控制器名字
 	c.SetOkStatus().Json("ok, UserLoginController: " + foo)
 	return nil
